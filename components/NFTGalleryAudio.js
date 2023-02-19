a3fdf645ebc2d60dd183c34fcd90e9d28aefd13a
NetCinematics <alpinefalcon@protonmail.com>
2023-02-18 17:22:04 -0800

    audio

--- components/NFTGalleryAudio.js
+++ components/NFTGalleryAudio.js
@@ -32,11 +32,36 @@ export default function GalleryAudioBook({ nfts, batch, batchKey }) {
     setBatch(IDX);
     if(batch){ batch.next(IDX); }
   }    
+  let loadingAudio = true;
+  // const elem = document.querySelector("audio");
+  // if(elem){
 
+  //   elem.addEventListener('canplay', (event) => {
+  //     loadingAudio = false;
+  //     console.log('canplay');
+  //   });
+
+  
+  //   elem.addEventListener('complete', (event) => {
+  //     loadingAudio = false;
+  //     console.log('complete');
+  //   });
+    
+  // }
   let nft = nfts[index];
+//this works on rarible
+//https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga
+//this is the broken path at nft.metadata.animation_url
+//ipfs://ipfs/bafybeiba57fsy6ywpfl2ewumydw76yfu3asgv6o5puaeaw4vowwpdnmopq/animation.mpga
+// debugger;
+let audioPath = (nft && nft.metadata)? nft.metadata.animation_url : null;
+if(audioPath){
+  audioPath = audioPath.replace('ipfs://','https://');
+  audioPath = audioPath.replace('ipfs/','ipfs.io/ipfs/');
+}
 return (
 
-<main className="h-full w-full" style={{'background-image':"url('../img/carbon_fibre2.png')"}}> 
+<main className="h-full w-full" style={{'backgroundImage':"url('../img/carbon_fibre2.png')"}}> 
 
   <div style={{color:'steelblue'}} className="h-full self-stretch items-stretch mb-1">        
   { (!nfts || nfts.length<1) ? <span>loading...</span>  :
@@ -57,9 +82,15 @@ return (
     {/* <audio controls="" src="ipfs://ipfs/bafybeiba57fsy6ywpfl2ewumydw76yfu3asgv6o5puaeaw4vowwpdnmopq/animation.mpga"></audio> */}
 {/* <audio controls="" src="https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga"></audio> */}
 
-      <audio controls src="https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga">
+      <audio preload="auto" controls complete={()=>{console.log('complete'); loadingAudio=false;} }
+        onCanPlay={()=>{console.log('canplay1'); loadingAudio=false;} }
+        canplay={()=>{console.log('canplay2'); loadingAudio=false;} }
+       src={ (audioPath)? audioPath :
+         'https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga'}>
       </audio>
-
+      {/* <audio controls src="https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga">
+      </audio> */}
+      {(loadingAudio)?'loading...':''}
 
      {/*//TODO:        <div data-player="true" data-url="https://ipfs.io/ipfs/bafybeihiwjkalxowd63ep4iia4nhjwbasqw654yfwpscku5oe47447h77q/animation.mpga" class="sc-bdvvtL sc-gsDKAQ sc-dkPtRN sc-bvZjhv LEuCB dqnTGd kryjMV"><button class="sc-bdvvtL sc-fKVqWL sc-bBHxTw sc-cxpSdN sc-llYSUQ sc-jJoQJp cDiMXI QejWU zgprO kyPRas dpuGUg sc-gyElHZ jmEuRo sc-rUGft bsquHD" type="button"><svg viewBox="0 0 24 24" fill="none" width="24" height="24" xlmns="http://www.w3.org/2000/svg" class="sc-bdvvtL sc-iCfMLu iWfNDX"><path d="M17.8187 10.5192C18.9527 11.1807 18.9527 12.8193 17.8187 13.4808L9.22096 18.4961C8.07812 19.1628 6.64289 18.3384 6.64289 17.0154L6.64289 6.98463C6.64289 5.66156 8.07812 4.83721 9.22096 5.50387L17.8187 10.5192Z" fill="currentColor"></path></svg></button><div class="sc-bdvvtL sc-gsDKAQ sc-dkPtRN sc-jfBFGL qfAKJ dqnTGd hYLlhE"><div class="sc-kKTuYo joxFld" style="width: 0%;"></div></div><button class="sc-bdvvtL sc-fKVqWL sc-bBHxTw sc-cxpSdN cDiMXI QejWU zgprO sc-zCoBu icPVYQ" type="button">4:57</button><button class="sc-bdvvtL sc-fKVqWL sc-bBHxTw sc-cxpSdN sc-llYSUQ sc-jJoQJp cDiMXI QejWU zgprO kyPRas dpuGUg sc-gyElHZ jmEuRo sc-rUGft bsquHD" type="button"><svg viewBox="0 0 24 24" fill="none" width="24" height="24" xlmns="http://www.w3.org/2000/svg" class="sc-bdvvtL sc-iCfMLu iWfNDX"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.2177 18.3421C14.7415 18.8879 14.1002 19.25 13.2931 19.25C12.5539 19.25 11.876 18.5925 11.4004 18.1037L9.44335 15.8205C9.3002 15.6535 9.22151 15.4408 9.22151 15.2208V8.07496C9.22151 7.85499 9.3002 7.64228 9.44335 7.47527L11.3891 5.20521C11.8543 4.66985 12.5322 4.04578 13.2931 4.04578C14.1002 4.04577 14.7415 4.40782 15.2177 4.9536C15.6768 5.47982 15.9865 6.17729 16.202 6.90648C16.6336 8.36714 16.7486 10.1741 16.7486 11.6479C16.7486 13.1216 16.6336 14.9286 16.202 16.3893C15.9865 17.1184 15.6768 17.8159 15.2177 18.3421ZM5 9.80494C5 8.91434 5.72197 8.19237 6.61257 8.19237H7.53403C7.66126 8.19237 7.7644 8.29551 7.7644 8.42274V14.873C7.7644 15.0002 7.66126 15.1034 7.53403 15.1034H6.61257C5.72197 15.1034 5 14.3814 5 13.4908V9.80494ZM19.6562 8.7955C19.6076 8.41692 19.2613 8.14943 18.8827 8.19803C18.5041 8.24664 18.2366 8.59294 18.2852 8.97152C18.3996 9.86268 18.44 10.7987 18.44 11.648C18.44 12.4972 18.3996 13.4332 18.2852 14.3244C18.2366 14.703 18.5041 15.0493 18.8827 15.0979C19.2613 15.1465 19.6076 14.879 19.6562 14.5004C19.7802 13.5342 19.8222 12.5364 19.8222 11.648C19.8222 10.7595 19.7802 9.76173 19.6562 8.7955Z" fill="currentColor"></path></svg></button></div>
       */}
